f6d7e67f4ef693a957a42af9add2e5683d935138
Geir Arne Hjelle <geir.arne.hjelle@xlent.no>
2025-08-11 13:43:31 +0200

    Upgrade to latest version of configaroo

--- src/galendar/config/__init__.py
+++ src/galendar/config/__init__.py
@@ -8,13 +8,16 @@ from configaroo import Configuration
 from galendar.config.schema import GalendarConfig
 
 with resources.path("galendar.config", "galendar.toml") as path:
-    config = Configuration.from_file(
-        path,
-        extra_dynamic={"__init__.cache_path": platformdirs.user_cache_dir("galendar")},
-        envs={
-            "GALENDAR_LOG_LEVEL": "log.level",
-            "DROPBOX_CLIENT_KEY": "dropbox.client_key",
-            "DROPBOX_CLIENT_SECRET": "dropbox.client_secret",
-            "DROPBOX_CLIENT_TOKEN": "dropbox.client_token",
-        },
-    ).with_model(model=GalendarConfig)
+    config = (
+        Configuration.from_file(path)
+        .parse_dynamic({"__init__.cache_path": platformdirs.user_cache_dir("galendar")})
+        .add_envs(
+            {
+                "GALENDAR_LOG_LEVEL": "log.level",
+                "DROPBOX_CLIENT_KEY": "dropbox.client_key",
+                "DROPBOX_CLIENT_SECRET": "dropbox.client_secret",
+                "DROPBOX_CLIENT_TOKEN": "dropbox.client_token",
+            }
+        )
+        .with_model(model=GalendarConfig)
+    )
