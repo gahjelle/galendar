ace81fb0dfc5f4f6ac0e03bcdd79137f704493ed
Geir Arne Hjelle <geir.arne.hjelle@xlent.no>
2025-08-15 09:01:50 +0200

    Move Galendar configuration file to root directory

--- src/galendar/config/__init__.py
+++ src/galendar/config/__init__.py
@@ -1,23 +1,21 @@
-"""Load galendar configuration."""
+"""Load Galendar configuration."""
 
-from importlib import resources
-
+import configaroo
 import platformdirs
-from configaroo import Configuration
 
 from galendar.config.schema import GalendarConfig
 
-with resources.path("galendar.config", "galendar.toml") as path:
-    config = (
-        Configuration.from_file(path)
-        .parse_dynamic({"__init__.cache_path": platformdirs.user_cache_dir("galendar")})
-        .add_envs(
-            {
-                "GALENDAR_LOG_LEVEL": "log.level",
-                "DROPBOX_CLIENT_KEY": "dropbox.client_key",
-                "DROPBOX_CLIENT_SECRET": "dropbox.client_secret",
-                "DROPBOX_CLIENT_TOKEN": "dropbox.client_token",
-            }
-        )
-        .with_model(model=GalendarConfig)
+path = configaroo.find_pyproject_toml() / "galendar.toml"
+config = (
+    configaroo.Configuration.from_file(path)
+    .add_envs(
+        {
+            "GALENDAR_LOG_LEVEL": "log.level",
+            "DROPBOX_CLIENT_KEY": "dropbox.client_key",
+            "DROPBOX_CLIENT_SECRET": "dropbox.client_secret",
+            "DROPBOX_CLIENT_TOKEN": "dropbox.client_token",
+        }
     )
+    .parse_dynamic({"__init__.cache_path": platformdirs.user_cache_dir("galendar")})
+    .with_model(model=GalendarConfig)
+)
